f42fb50b61f92e332f44a7b2eaafe94c296ba233
Karol S. Mazur <karol@mazur.me>
2018-11-07 21:03:13 -0500

    Update project structure

---
+++ Resources/HabitsFramework.h
@@ -0,0 +1,19 @@
+//
+//  HabitsFramework.h
+//  HabitsFramework
+//
+//  Created by Karol on 2018-11-07.
+//  Copyright © 2018 Karol S. Mazur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HabitsFramework.
+FOUNDATION_EXPORT double HabitsFrameworkVersionNumber;
+
+//! Project version string for HabitsFramework.
+FOUNDATION_EXPORT const unsigned char HabitsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HabitsFramework/PublicHeader.h>
+
+
