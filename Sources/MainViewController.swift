56bcf261e1d7c7e3f689dd6800ee2df87d9336ba
Karol S. Mazur <karol@mazur.me>
2018-11-07 22:30:44 -0500

    Add SwiftMessages to Main VC

--- Sources/MainViewController.swift
+++ Sources/MainViewController.swift
@@ -1,13 +1,25 @@
 import UIKit
+import SwiftMessages
 
 public class MainViewController: UITableViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Alert", style: .plain, target: self, action: #selector(MainViewController.alertAction))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Toast", style: .plain, target: self, action: #selector(MainViewController.toast))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Alert", style: .plain, target: self, action: #selector(MainViewController.alert))
+    }
+
+    @objc private dynamic func toast() {
+        let view = MessageView.viewFromNib(layout: .cardView)
+        view.configureTheme(.warning, iconStyle: .light)
+        (view.backgroundView as? CornerRoundingView)?.cornerRadius = 2
+        view.titleLabel?.isHidden = true
+        view.button?.isHidden = true
+        view.bodyLabel?.text = "Toast!"
+        SwiftMessages.show(view: view)
     }
     
-    @objc private dynamic func alertAction() {
+    @objc private dynamic func alert() {
         let alert = UIAlertController(title: "Alert", message: "This is an alert.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
